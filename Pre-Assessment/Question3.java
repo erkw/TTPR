edde1e521c65634a901d8fa0e36ade9f934e0f09
Eric Kwan <kwan.eric69@gmail.com>
2024-11-06 15:43:18 -0500

    Creates 2 strings and compares them if there is the same characters

--- Pre-Assessment/Question3.java
+++ Pre-Assessment/Question3.java
@@ -8,32 +8,6 @@ public class Question3 {
         for (int i = 0; i < str1.length(); i++) {
             char char1 = str1.charAt(i);
 
-            // Checks if the character in str1 is in the str2 and adds it to the commonLetters list
-            if (str2.indexOf(char1) != -1 && !commonLetters.contains(char1)) {
-                commonLetters.add(char1);
-            }
-        }
-
-        return commonLetters;
-    }
-
-    public static void main(String[] args) {
-        String str1 = "peter";
-        String str2 = "parker";
-        System.out.println("Common Letters: " + commonLetters(str1, str2));
-    }
-}
-
-import java.util.*;
-
-public class Question3 {
-    public static List<Character> commonLetters(String str1, String str2) {
-        List<Character> commonLetters = new ArrayList<>();
-        
-        // Loop through each character in the first string
-        for (int i = 0; i < str1.length(); i++) {
-            char char1 = str1.charAt(i);
-
             // Check if the character is in the second string and not already added
             if (str2.indexOf(char1) != -1 && !commonLetters.contains(char1)) {
                 commonLetters.add(char1);
